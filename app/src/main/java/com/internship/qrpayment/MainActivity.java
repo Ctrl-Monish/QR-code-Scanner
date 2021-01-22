6cb7c9afbcb5c84b35dd6b4412d6965987a69c60
Ctrl-Monish <monishmakkar.mm@gmail.com>
2021-01-23 04:09:37 +0530

    QR code scanner Android

---
+++ app/src/main/java/com/internship/qrpayment/MainActivity.java
@@ -0,0 +1,32 @@
+package com.internship.qrpayment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scanbtn;
+    public static TextView scantext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scanbtn = findViewById(R.id.scan_button);
+        scantext = findViewById(R.id.scan_textView);
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ScannerActivity.class));
+            }
+        });
+    }
+}
